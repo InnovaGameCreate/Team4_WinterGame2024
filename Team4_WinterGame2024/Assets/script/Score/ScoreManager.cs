f9dbcda8881c13157311606a8ca153d2127dfad8
WillowHa <liubensunmen@gmail.com>
2025-01-06 00:09:43 +0900

    Revert "gamesceneの変更など"
    This reverts commit b01de373244145796a4518f0d722cf592e3463f3.

--- Team4_WinterGame2024/Assets/script/Score/ScoreManager.cs
+++ Team4_WinterGame2024/Assets/script/Score/ScoreManager.cs
@@ -1,17 +1,24 @@
 using JetBrains.Annotations;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
-using UnityEngine.UI;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance;
     private int score;
-    public Text scoreText;
     // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 
     private void Awake()
     {
@@ -28,19 +35,5 @@ public class ScoreManager : MonoBehaviour
     {
         score += value;
         Debug.Log("Current Score" + score);
-    }
-
-    internal void DecreaseScore(int scoreValue)
-    {
-        score -= scoreValue;
-        Debug.Log("Current Score" + score);
-    }
-    private void UpdateScoreText()
-    {
-        // ƒXƒRƒA‚ð‰æ–Ê‚É”½‰f‚·‚é
-        if (scoreText != null)
-        {
-            scoreText.text = "Score: " + score;
-        }
     }
 }
