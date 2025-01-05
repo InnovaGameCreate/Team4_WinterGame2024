8c399cc8f6621ca43121165b0e13c7c6c59c49b3
WillowHa <liubensunmen@gmail.com>
2025-01-06 00:53:13 +0900

    Revert "コンフリクト防止gamescene"
    This reverts commit 5ad91d2e96edc10f30543d85c26e058405563d45.

--- Team4_WinterGame2024/Assets/script/Score/ScoreManager.cs
+++ Team4_WinterGame2024/Assets/script/Score/ScoreManager.cs
@@ -3,12 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance;
-<<<<<<< Updated upstream:Team4_WinterGame2024/Assets/script/Score/ScoreManager.cs
     private int score;
     // Start is called before the first frame update
     void Start()
@@ -21,10 +19,6 @@ public class ScoreManager : MonoBehaviour
     {
         
     }
-=======
-    public int score=0;
-    public Text scoreText;
->>>>>>> Stashed changes:Team4_WinterGame2024/Assets/script/Score/gamesceneplayer/ScoreManager.cs
 
     private void Awake()
     {
@@ -36,18 +30,10 @@ public class ScoreManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        UpdateScoreDisplay();
     }
-    public void UpdateScoreDisplay()
+    public void AddScore(int value)
     {
-<<<<<<< Updated upstream:Team4_WinterGame2024/Assets/script/Score/ScoreManager.cs
         score += value;
         Debug.Log("Current Score" + score);
-=======
-        if (scoreText != null)
-        {
-            scoreText.text = "Score: " + score.ToString();
-        }
->>>>>>> Stashed changes:Team4_WinterGame2024/Assets/script/Score/gamesceneplayer/ScoreManager.cs
     }
 }
